b8904cac9409ea517044be07ed6938c52232bc13
Shamir Khodzha <khodzha.sh@gmail.com>
2021-09-24 19:27:16 +0300

    metrics server

---
+++ examples/http_metrics.rs
@@ -0,0 +1,93 @@
+use std::sync::Arc;
+
+use axum::{extract, handler::get, AddExtensionLayer, Router};
+use foxford_ulms_svc_utils::metrics::MetricsServer;
+use futures::StreamExt;
+use prometheus::{IntCounter, IntGauge, Opts, Registry};
+use signal_hook::consts::TERM_SIGNALS;
+
+// Simple http server that collects two metrics (requests counter and inc - dec requests gauge) and three routes:
+//      /     - increases counter
+//      /inc  - increases counter and gauge
+//      /dec  - increases counter and decreases gauge
+// The servers listens on http://localhost:8080/, metrics are served from http://localhost:8081/metrics
+
+#[tokio::main]
+async fn main() {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "foxford_ulms_svc_utils::metrics=debug")
+    }
+    tracing_subscriber::fmt::init();
+
+    eprintln!("Hello world");
+    eprintln!("Listening on http://localhost:8080/\nAlso try visiting http://localhost:8080/inc and http://localhost:8080/dec");
+    eprintln!("Metrics on http://localhost:8081/metrics");
+
+    // Create a Counter.
+    let counter = {
+        let opts = Opts::new(
+            "total_requests_counter",
+            "Number of requests processed by the server",
+        );
+        IntCounter::with_opts(opts).unwrap()
+    };
+    let gauge = {
+        let opts = Opts::new(
+            "requests_gauge",
+            "Number of requests processed by the server",
+        );
+        IntGauge::with_opts(opts).unwrap()
+    };
+
+    let r = Registry::new();
+    r.register(Box::new(counter.clone())).unwrap();
+    r.register(Box::new(gauge.clone())).unwrap();
+
+    let shared_state = Arc::new(State(counter, gauge));
+
+    let metrics_server = MetricsServer::new(r, "0.0.0.0:8081".parse().unwrap());
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/inc", get(inc))
+        .route("/dec", get(dec))
+        .layer(AddExtensionLayer::new(shared_state));
+
+    let mut signals_stream = signal_hook_tokio::Signals::new(TERM_SIGNALS)
+        .unwrap()
+        .fuse();
+    let signals = signals_stream.next();
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(async {
+            signals.await;
+            eprintln!("\nServer shutting down...")
+        })
+        .await
+        .unwrap();
+
+    metrics_server.shutdown().await;
+    eprintln!("Goodbye");
+}
+
+struct State(IntCounter, IntGauge);
+
+async fn root(state: extract::Extension<Arc<State>>) -> &'static str {
+    state.0 .0.inc();
+    "Hello world"
+}
+
+async fn inc(state: extract::Extension<Arc<State>>) -> &'static str {
+    state.0 .0.inc();
+    state.0 .1.inc();
+
+    "Increased!"
+}
+
+async fn dec(state: extract::Extension<Arc<State>>) -> &'static str {
+    state.0 .0.inc();
+    state.0 .1.dec();
+
+    "Decreased!"
+}
---
+++ src/metrics.rs
@@ -0,0 +1,113 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::time::Duration;
+
+use axum::{
+    extract, handler,
+    routing::{EmptyRouter, Router},
+    AddExtensionLayer, Server,
+};
+use hyper::{Body, Request, Response};
+use prometheus::{Encoder, Registry, TextEncoder};
+use tokio::sync::oneshot;
+use tokio::task::JoinHandle;
+use tower_http::trace::TraceLayer;
+
+use tracing::{error, field::Empty, info, warn, Span};
+
+/// Http server with graceful shutdown that serves prometheus metrics
+///
+/// Runs in a separate tokio task
+pub struct MetricsServer {
+    join_handle: JoinHandle<Result<(), hyper::Error>>,
+    closer: oneshot::Sender<()>,
+}
+
+impl MetricsServer {
+    /// Create new server. This will spawn a new tokio task.
+    ///
+    /// # Arguments
+    ///
+    /// * `registry` - prometheus registry to gather metrics from
+    /// * `bind_addr` - address to bind server to
+    pub fn new(registry: Registry, bind_addr: SocketAddr) -> Self {
+        let (closer, rx) = oneshot::channel::<()>();
+
+        let app: Router<EmptyRouter<Infallible>> = Router::new();
+
+        let app = app
+            .route("/metrics", handler::get(metrics_handler))
+            .layer(AddExtensionLayer::new(registry))
+            .layer(
+                TraceLayer::new_for_http()
+                    .make_span_with(|request: &Request<_>| {
+                        // TODO: Option will be recorded simpler
+                        // when https://github.com/tokio-rs/tracing/pull/1393 lands
+
+                        let span = tracing::info_span!(
+                            "http-metrics-request",
+                            status_code = Empty,
+                            path = request.uri().path(),
+                            query = Empty
+                        );
+                        if let Some(query) = request.uri().query() {
+                            span.record("query", &query);
+                        }
+                        span
+                    })
+                    .on_response(|response: &Response<_>, latency: Duration, span: &Span| {
+                        span.record("status_code", &tracing::field::display(response.status()));
+                        info!("response generated in {:?}", latency)
+                    }),
+            );
+
+        let join_handle = tokio::task::spawn(async move {
+            Server::bind(&bind_addr)
+                .serve(app.into_make_service())
+                .with_graceful_shutdown(async {
+                    rx.await.ok();
+                })
+                .await
+        });
+
+        Self {
+            join_handle,
+            closer,
+        }
+    }
+
+    /// Shutdowns the server
+    pub async fn shutdown(self) {
+        info!("Received signal, triggering metrics server shutdown");
+
+        let _ = self.closer.send(());
+        let fut = tokio::time::timeout(Duration::from_secs(3), self.join_handle);
+
+        match fut.await {
+            Err(e) => {
+                error!("Metrics server timed out during shutdown, error = {:?}", e);
+            }
+            Ok(Err(e)) => {
+                error!("Metrics server failed during shutdown, error = {:?}", e);
+            }
+            Ok(Ok(_)) => {
+                info!("Metrics server successfully exited");
+            }
+        }
+    }
+}
+
+async fn metrics_handler(state: extract::Extension<Registry>) -> Response<Body> {
+    let registry = state.0;
+    let mut buffer = vec![];
+    let encoder = TextEncoder::new();
+    let metric_families = registry.gather();
+    let response = match encoder.encode(&metric_families, &mut buffer) {
+        Ok(_) => Response::builder().status(200).body(buffer.into()).unwrap(),
+        Err(err) => {
+            warn!("Metrics not gathered: {:?}", err);
+            Response::builder().status(500).body(vec![].into()).unwrap()
+        }
+    };
+    response
+}
