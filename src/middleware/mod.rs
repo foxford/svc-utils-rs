3ae0d1f818e6c9377c24a212ab43999104b8eb90
Shamir Khodzha <khodzha.sh@gmail.com>
2021-11-10 17:35:55 +0300

    changed cors middleware to exclude * header values
    these values ruin browser caching for preflight requests

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,5 +1,5 @@
 #[cfg(feature = "cors-middleware")]
-pub use cors::{MiddlewareLayer as CorsLayer, Middleware as CorsMiddleware};
+pub use cors::{Middleware as CorsMiddleware, MiddlewareLayer as CorsLayer};
 
 #[cfg(feature = "cors-middleware")]
 mod cors;
