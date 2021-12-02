8a7c2f34c1446403e8bc8282f62b2ea3dc7eebce
Shamir Khodzha <khodzha.sh@gmail.com>
2021-12-02 15:38:28 +0300

    added log middleware

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,5 +1,11 @@
 #[cfg(feature = "cors-middleware")]
 pub use cors::{Middleware as CorsMiddleware, MiddlewareLayer as CorsLayer};
 
+#[cfg(feature = "log-middleware")]
+pub use log::LogLayer;
+
 #[cfg(feature = "cors-middleware")]
 mod cors;
+
+#[cfg(feature = "log-middleware")]
+mod log;
