acadd7b13a4b31bba037eb1fab4e5f01a1b32413
Dmitry Shlagoff <richyannet@yandex.ru>
2022-07-12 13:45:22 +0200

    Fix authn extractor rejection content type

--- src/extractors/authn.rs
+++ src/extractors/authn.rs
@@ -3,35 +3,36 @@ use std::sync::Arc;
 use axum::{
     async_trait,
     body::Body,
-    extract::{FromRequest, RequestParts},
+    extract::{FromRequest, RequestParts, Json},
 };
 use http::StatusCode;
 use svc_agent::{AccountId, AgentId};
 use svc_authn::jose::ConfigMap as AuthnConfig;
 use svc_authn::token::jws_compact::extract::decode_jws_compact_with_config;
+use svc_error::Error;
 use tracing::{field, Span};
 
 pub struct Extractor(pub AgentId);
 
 #[async_trait]
 impl FromRequest<Body> for Extractor {
-    type Rejection = (StatusCode, &'static str);
+    type Rejection = (StatusCode, Json<Error>);
 
     async fn from_request(req: &mut RequestParts<Body>) -> Result<Self, Self::Rejection> {
         let authn = req
             .extensions()
             .get::<Arc<AuthnConfig>>()
-            .ok_or((StatusCode::UNAUTHORIZED, "No authn config"))?;
+            .ok_or((StatusCode::UNAUTHORIZED, Json(Error::new("no_authn_config", "No authn config", StatusCode::UNAUTHORIZED))))?;
 
         let auth_header = req
             .headers()
             .get("Authorization")
             .and_then(|x| x.to_str().ok())
             .and_then(|x| x.get("Bearer ".len()..))
-            .ok_or((StatusCode::UNAUTHORIZED, "Invalid authentication"))?;
+            .ok_or((StatusCode::UNAUTHORIZED, Json(Error::new("invalid_authentication", "Invalid authentication", StatusCode::UNAUTHORIZED))))?;
 
         let claims = decode_jws_compact_with_config::<String>(auth_header, authn)
-            .map_err(|_| (StatusCode::UNAUTHORIZED, "Invalid authentication"))?
+            .map_err(|_| (StatusCode::UNAUTHORIZED, Json(Error::new("invalid_authentication", "Invalid authentication", StatusCode::UNAUTHORIZED))))?
             .claims;
         let account = AccountId::new(claims.subject(), claims.audience());
 
